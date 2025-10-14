98404844f742a2526aee8d76309b5c53a3968d97
David Yeo <david.j.yeo@outlook.com>
2025-10-14 15:33:26 +0100

    absolutio paths for components

--- src/terraform/shared/keyvault.tf
+++ src/terraform/shared/keyvault.tf
@@ -1,11 +1,10 @@
 resource "azurerm_key_vault" "main" {
 
-  name                = "kv-${random_string.suffix.result}"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
-
+  name                          = "kv-wibble"
+  location                      = azurerm_resource_group.main.location
+  resource_group_name           = azurerm_resource_group.main.name
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  sku_name                      = "standard"
   purge_protection_enabled      = false
   soft_delete_retention_days    = 7
   public_network_access_enabled = true
